5206455f4a8447cff51f840a38fcea54ffefff40
truongdn <truongdn@fabbi.io>
2021-04-13 13:47:58 +0700

    refactor: delete next auth

---
+++ components/forms/logout.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const NameAcc = styled.span`
+  font-weight: 600;
+`;
+export const AvarIcon = styled.span`
+  cursor: pointer;
+`;
+export const Hr = styled.hr`
+  background: #dadde1;
+  border-width: 0;
+  color: #dadde1;
+  height: 1px;
+`;
