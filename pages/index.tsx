d9aa01a0c9dbb88cfbe2859d08b7607acdf1a8af
truongdn <truongdn@fabbi.io>
2021-04-13 13:47:58 +0700

    refactor: delete next auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
 import MainLayout from '@layouts/main';
-import { signIn } from 'next-auth/client';
 import { withAuthSSP } from '@utils/auth/index';
+import { Logout } from 'interface/logout';
 
-const Home = () => (
-  <MainLayout>
+const Home = ({ user }: Logout) => (
+  <MainLayout user={user}>
     <h1>Bảng điều khiển</h1>
-    <button type="button" onClick={() => signIn()}>
-      Dang nhap
-    </button>
   </MainLayout>
 );
 
